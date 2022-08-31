65a352d5352f046b2c9981f801baa9c7784055c3
Fabian Keunecke <f.keunecke@liveyourproject.com>
2022-08-31 11:24:39 +0200

    Close filter instead of whole app by using native back button

--- lib/filter_dialog.dart
+++ lib/filter_dialog.dart
@@ -27,114 +27,131 @@ class FilterDialogState extends State<FilterDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            color: Theme.of(context).scaffoldBackgroundColor,
-            child: Padding(
-                padding:
-                    EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.close),
-                        onPressed: () {
-                          setState(() {
-                            widget.onCancel();
-                          });
-                        },
-                      ),
-                      Expanded(
-                          child: SingleChildScrollView(
-                              child: Padding(
-                                  padding: EdgeInsets.all(16.0),
-                                  child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Text("Spritsorte",
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold)),
-                                        Padding(
-                                            padding: EdgeInsets.only(top: 8),
-                                            child: Wrap(
-                                              spacing: 8,
-                                              children: <Widget>[
-                                                FilterChip(
-                                                    label: Text("Super E5",
-                                                        style: TextStyle(
-                                                            fontSize: 16)),
-                                                    avatar: CircleAvatar(
-                                                        backgroundColor:
-                                                            Theme.of(context)
+    return WillPopScope(
+        onWillPop: () {
+          widget.onCancel();
+          return Future.value(false);
+        },
+        child: Scaffold(
+            body: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: Padding(
+                    padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).padding.top),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.close),
+                            onPressed: () {
+                              setState(() {
+                                widget.onCancel();
+                              });
+                            },
+                          ),
+                          Expanded(
+                              child: SingleChildScrollView(
+                                  child: Padding(
+                                      padding: EdgeInsets.all(16.0),
+                                      child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Text("Spritsorte",
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight:
+                                                        FontWeight.bold)),
+                                            Padding(
+                                                padding:
+                                                    EdgeInsets.only(top: 8),
+                                                child: Wrap(
+                                                  spacing: 8,
+                                                  children: <Widget>[
+                                                    FilterChip(
+                                                        label: Text("Super E5",
+                                                            style: TextStyle(
+                                                                fontSize: 16)),
+                                                        avatar: CircleAvatar(
+                                                            backgroundColor: Theme
+                                                                    .of(context)
                                                                 .primaryColor),
-                                                    selected: _gas == "e5",
-                                                    onSelected: (bool value) {
-                                                      setState(() {
-                                                        _gas = "e5";
-                                                      });
-                                                    }),
-                                                FilterChip(
-                                                    label: Text("Super E10",
-                                                        style: TextStyle(
-                                                            fontSize: 16)),
-                                                    avatar: CircleAvatar(
-                                                        backgroundColor:
-                                                            Theme.of(context)
+                                                        selected: _gas == "e5",
+                                                        onSelected:
+                                                            (bool value) {
+                                                          setState(() {
+                                                            _gas = "e5";
+                                                          });
+                                                        }),
+                                                    FilterChip(
+                                                        label: Text("Super E10",
+                                                            style: TextStyle(
+                                                                fontSize: 16)),
+                                                        avatar: CircleAvatar(
+                                                            backgroundColor: Theme
+                                                                    .of(context)
                                                                 .primaryColor),
-                                                    selected: _gas == "e10",
-                                                    onSelected: (bool value) {
-                                                      setState(() {
-                                                        _gas = "e10";
-                                                      });
-                                                    }),
-                                                FilterChip(
-                                                    label: Text("Diesel",
-                                                        style: TextStyle(
-                                                            fontSize: 16)),
-                                                    avatar: CircleAvatar(
-                                                        backgroundColor:
-                                                            Theme.of(context)
+                                                        selected: _gas == "e10",
+                                                        onSelected:
+                                                            (bool value) {
+                                                          setState(() {
+                                                            _gas = "e10";
+                                                          });
+                                                        }),
+                                                    FilterChip(
+                                                        label: Text("Diesel",
+                                                            style: TextStyle(
+                                                                fontSize: 16)),
+                                                        avatar: CircleAvatar(
+                                                            backgroundColor: Theme
+                                                                    .of(context)
                                                                 .primaryColor),
-                                                    selected: _gas == "diesel",
-                                                    onSelected: (bool value) {
-                                                      setState(() {
-                                                        _gas = "diesel";
-                                                      });
-                                                    }),
-                                              ],
-                                            )),
-                                        Padding(
-                                            padding: EdgeInsets.only(top: 32),
-                                            child: SizedBox(
-                                                width: double.infinity,
-                                                child: Row(children: [
-                                                  Expanded(
-                                                      child: Padding(
-                                                    padding: EdgeInsets.only(
-                                                        right: 8.0),
-                                                    child: Container(),
-                                                  )),
-                                                  Expanded(
-                                                      child: Padding(
-                                                    padding: EdgeInsets.only(
-                                                        left: 8.0),
-                                                    child: ElevatedButton(
-                                                      onPressed: () {
-                                                        final filter = Filter(
-                                                          _gas,
-                                                        );
-                                                        widget.onSubmit(filter);
-                                                      },
-                                                      child: Text("Speichern"),
-                                                    ),
-                                                  ))
-                                                ])))
-                                      ]))))
-                    ]))));
+                                                        selected:
+                                                            _gas == "diesel",
+                                                        onSelected:
+                                                            (bool value) {
+                                                          setState(() {
+                                                            _gas = "diesel";
+                                                          });
+                                                        }),
+                                                  ],
+                                                )),
+                                            Padding(
+                                                padding:
+                                                    EdgeInsets.only(top: 32),
+                                                child: SizedBox(
+                                                    width: double.infinity,
+                                                    child: Row(children: [
+                                                      Expanded(
+                                                          child: Padding(
+                                                        padding:
+                                                            EdgeInsets.only(
+                                                                right: 8.0),
+                                                        child: Container(),
+                                                      )),
+                                                      Expanded(
+                                                          child: Padding(
+                                                        padding:
+                                                            EdgeInsets.only(
+                                                                left: 8.0),
+                                                        child: ElevatedButton(
+                                                          onPressed: () {
+                                                            final filter =
+                                                                Filter(
+                                                              _gas,
+                                                            );
+                                                            widget.onSubmit(
+                                                                filter);
+                                                          },
+                                                          child:
+                                                              Text("Speichern"),
+                                                        ),
+                                                      ))
+                                                    ])))
+                                          ]))))
+                        ])))));
   }
 }
 
