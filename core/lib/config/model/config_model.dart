c9a19df8c172a0a7535a4bf9507b88feaa10373a
Fabian Keunecke <f@biankeunecke.de>
2023-11-09 11:03:46 +0100

    Update dependencies

--- core/lib/config/model/config_model.dart
+++ core/lib/config/model/config_model.dart
@@ -2,14 +2,22 @@ class ConfigModel {
   final bool useOpenStreetMap;
   final String googleMapsKey;
   final String tankerkoenigApiKey;
+  final String mapLibreStyleUrlLight;
+  final String mapLibreStyleUrlDark;
 
-  ConfigModel(this.useOpenStreetMap, this.googleMapsKey, this.tankerkoenigApiKey);
+  ConfigModel(
+      this.useOpenStreetMap,
+      this.googleMapsKey,
+      this.tankerkoenigApiKey,
+      this.mapLibreStyleUrlLight,
+      this.mapLibreStyleUrlDark);
 
   factory ConfigModel.fromJson(Map<String, dynamic> parsedJson) {
     return ConfigModel(
         parsedJson['tankste']['useOpenStreetMap'] ?? true,
         parsedJson['google']['mapsKey'] ?? "",
-        parsedJson['tankerKoenig']['apiKey'] ?? ""
-    );
+        parsedJson['tankerKoenig']['apiKey'] ?? "",
+        parsedJson['mapLibre']['styleUrlLight'] ?? "",
+        parsedJson['mapLibre']['styleUrlDark'] ?? "");
   }
 }
