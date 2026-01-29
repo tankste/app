a6b08ae4eb848b17e81e7a050c89dc9c3be609a8
Fabian Keunecke <github@fabiankeunecke.de>
2026-01-29 18:44:33 +0100

    Change packages to FOSS #86 (#1)
    * Change packages to FOSS
    
    * Set configuration

--- get-deps.sh
+++ get-deps.sh
@@ -8,21 +8,17 @@ function pub_get () {
 
 pub_get core
 pub_get sponsor/core
-pub_get sponsor/data_closed
 pub_get sponsor/data_foss
 pub_get sponsor/ui
 pub_get settings
 pub_get report
 pub_get map/core
 pub_get map/maplibre
-pub_get map/apple_closed
 pub_get map/apple_foss
-pub_get map/google_closed
 pub_get map/google_foss
 pub_get map/map
 pub_get sponsor/ui
 pub_get navigation/core
-pub_get navigation/impl_google
 pub_get navigation/impl_foss
 pub_get navigation/navigation
 pub_get station
